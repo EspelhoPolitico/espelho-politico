6b2a03fc74ecfe79d580434dfebbfa40c64d884b
Wilton Rodrigues <braynwilton@gmail.com>
2014-09-24 22:19:10 -0300

    Corrigindo identações

--- db/migrate/20140924155754_criar_tipo_proposicao.rb
+++ db/migrate/20140924155754_criar_tipo_proposicao.rb
@@ -1,8 +1,8 @@
 class CriarProposicao < ActiveRecord::Migration
   def change
-		create_table	:tipo_proposicao do |t|
-			t.string		:sigla      
-			t.string		:descricao
-		end
-	end
+    create_table	:tipo_proposicao do |t|
+      t.string		:sigla      
+      t.string		:descricao
+    end
+  end
 end
