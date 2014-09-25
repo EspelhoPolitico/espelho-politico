a527d1d688b07008af983462d929a7b9502c59ec
Wilton Rodrigues <braynwilton@gmail.com>
2014-09-25 16:38:39 -0300

    Removendo 'lixo' do arquivo

--- app/models/Proposition.rb
+++ app/models/Proposition.rb
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 class Proposition < ActiveRecord::Base
   belongs_to :Parliamentarian
 	
   validates :id, presence: true, length: {maximum: 11}
 end
-=======
-class Proposicao < ActiveRecord::Base
-  belongs_to :parlamentar
-	
-  validates :id, presence: true, length: {maximum: 11}
-end
->>>>>>> 1e0dce01d676409e281880ac0f84fbe81cb526e3
