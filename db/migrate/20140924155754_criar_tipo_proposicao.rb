2a01428e5f42e92fa496d773c44a646adb1abd12
Wilton Rodrigues <braynwilton@gmail.com>
2014-09-25 14:31:24 -0300

    Delete 20140924155754_criar_tipo_proposicao.rb

--- db/migrate/20140924155754_criar_tipo_proposicao.rb
+++
@@ -1,8 +0,0 @@
-class CriarProposicao < ActiveRecord::Migration
-  def change
-    create_table	:tipo_proposicao do |t|
-      t.string		:sigla      
-      t.string		:descricao
-    end
-  end
-end
