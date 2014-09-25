ab76b36e35d0564f5b42d01b3ec016598081698e
Wilton Rodrigues <braynwilton@gmail.com>
2014-09-25 15:13:46 -0300

    Refatorando atributos da Migrate para ingles

--- db/migrate/20140920204051_criar_proposicao.rb
+++ db/migrate/20140920204051_criar_proposicao.rb
@@ -1,19 +1,20 @@
-class CriarProposicao < ActiveRecord::Migration
+class CreateProposition < ActiveRecord::Migration
   def change
-    create_table :proposicao do |t|
-      t.integer      :ano
-      t.integer      :numero
-      t.text         :ementa
-      t.text         :explicacao
-      t.string       :tema
-      t.references   :parlamentar
-      t.date         :data_apresentacao
-      t.string       :situacao
-      t.string       :link_teor
+    create_table :proposition do |t|
+      t.integer      :year
+      t.integer      :number
+      t.text         :amendment
+      t.text         :explanation
+      t.string       :theme
+      t.references   :Parliamentarian
+      t.references   :TypeProposition
+      t.date         :presentation_date
+      t.string       :situation
+      t.string       :content_link
     end
     add_index(
-      :proposicao, 
-      [:numero, :ano, :parlamentar_id],
+      :proposition, 
+      [:number, :year, :Parliamentarian_id],
       unique: true
     )
   end
