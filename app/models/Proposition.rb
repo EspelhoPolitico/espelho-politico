3c1705e66b3d4ac3afa0d9d6b418bb1090c52a31
Wilton Rodrigues <braynwilton@gmail.com>
2014-09-25 16:25:46 -0300

    Corrigindo referência para model Parliamentarian

--- app/models/Proposition.rb
+++ app/models/Proposition.rb
@@ -1,5 +1,13 @@
+<<<<<<< HEAD
 class Proposition < ActiveRecord::Base
-  belongs_to :Parlamentarian
+  belongs_to :Parliamentarian
 	
   validates :id, presence: true, length: {maximum: 11}
 end
+=======
+class Proposicao < ActiveRecord::Base
+  belongs_to :parlamentar
+	
+  validates :id, presence: true, length: {maximum: 11}
+end
+>>>>>>> 1e0dce01d676409e281880ac0f84fbe81cb526e3
