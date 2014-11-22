f1f65277a0180650cdf4e7d09e7342a95778fc23
Kleber Brito Moreira <kleberbritomoreira10@gmail.com>
2014-11-22 14:19:23 -0200

    Adicionando as index para o encadeamento

--- db/migrate/20141122161409_create_comment_hierarchies.rb
+++ db/migrate/20141122161409_create_comment_hierarchies.rb
@@ -5,4 +5,14 @@ class CreateCommentHierarchies < ActiveRecord::Migration
       t.integer  :descendant_id, :null => false # ID of the target comment
       t.integer  :generations, :null => false   # Number of generations between the ancestor and the descendant. Parent/child = 1, for example.
     end
+
+  # For "all progeny of…" and leaf selects:
+    add_index :comment_hierarchies, [:ancestor_id, :descendant_id, :generations],
+              :unique => true, :name => "comment_anc_desc_udx"
+ 
+    # For "all ancestors of…" selects,
+    add_index :comment_hierarchies, [:descendant_id],
+              :name => "comment_desc_idx"
+  end
+
 end
