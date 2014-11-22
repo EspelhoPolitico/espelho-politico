34572477c6c61899c11ab0e0cb2aa61ec30571dd
Kleber Brito Moreira <kleberbritomoreira10@gmail.com>
2014-11-22 14:18:25 -0200

    Criação da tabela com os campos ancestor, descendant, generations

--- db/migrate/20141122161409_create_comment_hierarchies.rb
+++ db/migrate/20141122161409_create_comment_hierarchies.rb
@@ -1,6 +1,8 @@
 class CreateCommentHierarchies < ActiveRecord::Migration
   def change
-    create_table :comment_hierarchies do |t|
+    create_table :comment_hierarchies, :id => false do |t|
+      t.integer  :ancestor_id, :null => false   # ID of the parent/grandparent/great-grandparent/... comments
+      t.integer  :descendant_id, :null => false # ID of the target comment
+      t.integer  :generations, :null => false   # Number of generations between the ancestor and the descendant. Parent/child = 1, for example.
     end
-  end
 end
