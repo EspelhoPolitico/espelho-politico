0ae22e144d73d284b7f502da1716dbc0fd9d3df6
Kleber Brito Moreira <kleberbritomoreira10@gmail.com>
2014-11-26 09:58:05 -0200

    Ajustando a view e os detalhes da funcionalidade dos comentários

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,7 @@
 class Comment < ActiveRecord::Base
-	acts_as_tree order: 'created_at DESC'
+  acts_as_tree order: 'created_at DESC'
+
+  validates_presence_of :author, :body, message: 'Campo obrigatório'   
+  validates_length_of :body, :maximum => 800, :on => [:create], message: 'Formato inválido' 
+
 end
